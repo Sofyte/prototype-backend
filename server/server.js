0cebd2bc2ba4a6d556d0fcedeb45f83df8f7a183
Sofyte <sofisokolovaite@gmail.com>
2026-01-19 23:01:20 +0200

    server fix

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ const db = mysql.createConnection({
   port: Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306),
   user: process.env.DB_USER || process.env.MYSQLUSER,
   password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE,
   multipleStatements: true,
 });
 
